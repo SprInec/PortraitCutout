b7368fa7a15586f594d1dbfa3b2ae7c9f6fea7a9
July <JulyCub@163.com>
2024-02-20 20:11:40 +0800

    update

--- Scripts/main.py
+++ Scripts/main.py
@@ -14,12 +14,16 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+from PyQt5 import QtCore
+
 
 import backend
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
+    MainWindow.setWindowIcon(QIcon('./icon.png'))
     eventProcress = backend.EventProcress(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
