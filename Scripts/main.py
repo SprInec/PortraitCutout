9bd0e9a9d63434d294ec4443c486f2ffe7dc043d
SprInec <JulyCub@163.com>
2024-10-06 03:23:03 +0800

    add qt-material theme

--- Scripts/main.py
+++ Scripts/main.py
@@ -6,7 +6,7 @@
 # Author: Sprine 
 # Email: None
 # Software: VSCode
-# Version: 1.0
+# Version: 1.1 2024.10.06
 #
 # Desc: None
 #
@@ -16,7 +16,9 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore
-from qt_material import apply_stylesheet
+
+# TODO:主题包, 若使用请先安装: pip install qt-material
+from qt_material import apply_stylesheet # 若使用默认样式请注释该行
 
 
 import backend
@@ -26,7 +28,15 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     MainWindow = QMainWindow()
     MainWindow.setWindowIcon(QIcon('./icon.png'))
-    apply_stylesheet(app, theme='dark_lightgreen.xml')
+    
+    """主题包, 可选
+        dark_blue.xml, dark_cyan.xml, dark_lightgreen.xml, dark_pink.xml, 
+        dark_purple.xml, dark_red.xml, dark_teal.xml, 
+        light_blue.xml, light_cyan.xml, light_green.xml, light_pink.xml, 
+        light_purple.xml, light_red.xml, light_teal.xml
+    """
+    apply_stylesheet(app, theme='dark_lightgreen.xml') # 若使用默认样式则注释该行
+    
     eventProcress = backend.EventProcress(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
